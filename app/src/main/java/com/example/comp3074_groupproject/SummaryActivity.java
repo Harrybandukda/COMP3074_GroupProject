05dbd929b146f15e6e9c31cf1fcf95271c988603
n2milmin <nicoleamilmine@gmail.com>
2024-11-22 10:42:40 -0500

    Base design for Summary screen. A few errors but nothing I'm concerned about rn

--- app/src/main/java/com/example/comp3074_groupproject/SummaryActivity.java
+++ app/src/main/java/com/example/comp3074_groupproject/SummaryActivity.java
@@ -1,4 +1,17 @@
 package com.example.comp3074_groupproject;
 
-public class SummaryActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+
+
 }
