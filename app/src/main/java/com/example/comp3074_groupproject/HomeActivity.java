9603e91fd73ea267432e853c05676ace66ac2504
Oleg Chystieiev <oleg.chisteev@gmail.com>
2024-11-22 11:17:37 -0500

    All basic views created

---
+++ app/src/main/java/com/example/comp3074_groupproject/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.comp3074_groupproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void openMealPlanner(View view) {
+        startActivity(new Intent(this, MealPlannerActivity.class));
+    }
+
+    public void openProfile(View view) {
+        startActivity(new Intent(this, ProfileActivity.class));
+    }
+
+    public void openHistory(View view) {
+        startActivity(new Intent(this, HistoryActivity.class));
+    }
+}
---
+++ app/src/main/java/com/example/comp3074_groupproject/MealPlannerActivity.java
@@ -0,0 +1,34 @@
+package com.example.comp3074_groupproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+
+public class MealPlannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_meal_planner);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void openDailyPlan(View view) {
+        startActivity(new Intent(this, DailyPlanActivity.class));
+    }
+
+    public void openLogin(View view) {
+        startActivity(new Intent(this, LogInActivity.class));
+    }
+}
