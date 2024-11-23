9ae605b470ed96dd1aaa8f4371aa9cc57b2ffbe8
n2milmin <nicoleamilmine@gmail.com>
2024-11-22 22:39:54 -0500

    Working UI for Profile, Edit Profile, Edit Dietary Preferences, Edit Allergies & Restrictions. All that's left is the Settings page.
    10 errors & 55 warnings, but it runs fine and most of the errors are because the touch area is too small

--- app/src/main/java/com/example/comp3074_groupproject/ProfileActivity.java
+++ app/src/main/java/com/example/comp3074_groupproject/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.comp3074_groupproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,47 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void toEditProfile(View view) {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void toSettings(View view) {
+        // no setting atm
+    }
+
+    public void toEditPreferences(View view) {
+        Intent intent = new Intent(this, EditDietPrefActivity.class);
+        startActivity(intent);
+    }
+
+    public void toEditAllergies(View view) {
+        Intent intent = new Intent(this, EditAllergiesActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    // Navigation bar methods
+    public void navigateToHome(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToMealPlanner(View view) {
+        Intent intent = new Intent(this, MealPlannerActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToProfile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToHistory(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
     }
 }
