45c508403fa786530e420fcea2fed802a275fa5e
AnnaShiba <102438674+AnnaShiba@users.noreply.github.com>
2024-12-08 17:51:24 -0500

    Get recipe details from the server

--- app/src/main/java/com/example/comp3074_groupproject/Adapters/RandomRecipeAdapter.java
+++ app/src/main/java/com/example/comp3074_groupproject/Adapters/RandomRecipeAdapter.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.comp3074_groupproject.Listerners.DetailClickListener;
 import com.example.comp3074_groupproject.Models.Recipe;
 import com.example.comp3074_groupproject.R;
 import com.squareup.picasso.Picasso;
@@ -21,11 +22,13 @@ public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHo
 
     Context context;
     List<Recipe> list;
+    DetailClickListener listener;
 
 
-    public RandomRecipeAdapter(Context context, List<Recipe> list) {
+    public RandomRecipeAdapter(Context context, List<Recipe> list, DetailClickListener listener) {
         this.context = context;
         this.list = list;
+        this.listener = listener;
     }
 
     @NonNull
@@ -44,6 +47,14 @@ public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHo
         holder.textView_servings.setText(list.get(position).servings+" Servings");
         holder.textView_duration.setText(list.get(position).readyInMinutes+" Mints");
         Picasso.get().load(list.get(position).image).into(holder.imageView_food);
+
+        holder.random_list_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onRecipeClicked(String.valueOf
+                        (list.get(holder.getAdapterPosition()).id));
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/comp3074_groupproject/RequestManager.java
+++ app/src/main/java/com/example/comp3074_groupproject/RequestManager.java
@@ -3,7 +3,9 @@ package com.example.comp3074_groupproject;
 import android.content.Context;
 
 import com.example.comp3074_groupproject.Listerners.RandomRecipeListener;
+import com.example.comp3074_groupproject.Listerners.RecipeDetailListener;
 import com.example.comp3074_groupproject.Models.RandomRecipeResponse;
+import com.example.comp3074_groupproject.Models.RecipeDetailResponse;
 
 import java.util.List;
 
@@ -13,6 +15,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public class RequestManager {
@@ -50,12 +53,44 @@ public class RequestManager {
         });
     }
 
+    public void getDetails(RecipeDetailListener listener, int id) {
+        RecipeDetails callRecipeDetails = retrofit.create(RecipeDetails.class);
+        Call<RecipeDetailResponse> call =
+                callRecipeDetails.callDetails(id, context
+                        .getString(R.string.api_key));
+
+        call.enqueue(new Callback<RecipeDetailResponse>() {
+            @Override
+            public void onResponse(Call<RecipeDetailResponse> call, Response<RecipeDetailResponse> response) {
+                if(!response.isSuccessful()){
+                    listener.didError(response.message());
+                    return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(Call<RecipeDetailResponse> call, Throwable t) {
+                listener.didError(t.getMessage());
+            }
+        });
+    }
+
     private interface CallRandomRecipes{
         @GET("recipes/random")
         Call<RandomRecipeResponse> callRandomRecipe(
                 @Query("apiKey") String apikey,
                 @Query("number") String number,
                 @Query("tags") List<String> tags
+        );
+    }
+
+    private interface RecipeDetails{
+        @GET("recipes/{id}/information")
+        Call<RecipeDetailResponse> callDetails(
+                @Path("id") int id,
+                @Query("apiKey") String apikey
+
         );
     }
 }
